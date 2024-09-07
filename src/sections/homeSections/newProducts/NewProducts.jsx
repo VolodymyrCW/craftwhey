bcab9a10eb6e3c481e2736729efdb3fb13df1535
Maksimchik36 <Peltek1985@gmail.com>
2024-09-07 20:35:23 +0300

    changed id for #

--- src/sections/homeSections/newProducts/NewProducts.jsx
+++ src/sections/homeSections/newProducts/NewProducts.jsx
@@ -3,7 +3,7 @@ import styles from "./NewProducts.module.scss";
 
 const NewProducts = () => {
   return (
-    <section id="newProducts">
+    <section id="new-products">
       <div className={`container`}>
         <h2 className={styles.title}>Новинка!</h2>
       </div>
