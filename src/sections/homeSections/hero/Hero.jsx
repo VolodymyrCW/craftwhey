610905bb3a2df2cc16332d26e926cb3472b0fdb5
IvetaDuda <92565279+IvetaDuda@users.noreply.github.com>
2024-10-03 00:25:02 +0300

    fix

--- src/sections/homeSections/hero/Hero.jsx
+++ src/sections/homeSections/hero/Hero.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useEffect, useContext } from 'react';
+import { useState, useEffect } from 'react';
 import Image from 'next/image';
 
 import { productsCategory } from '@/data/productsCategory';
