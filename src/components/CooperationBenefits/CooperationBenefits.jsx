32c48b21b7430a18ef2571dc5998c530157b4a27
PolztiukVolodymyr <92870556+PolztiukVolodymyr@users.noreply.github.com>
2024-10-10 17:04:43 +0300

    add tablet styles

--- src/components/CooperationBenefits/CooperationBenefits.jsx
+++ src/components/CooperationBenefits/CooperationBenefits.jsx
@@ -1,7 +1,24 @@
-// import styles from "./CooperationBenefits.module.scss";
+import { benefitsData } from "@/data/cooperationBenefits";
+import styles from "./CooperationBenefits.module.scss";
 
 const CooperationBenefits = () => {
-    return <div>Cooperation Benefits</div>;
+    return (
+        <ul className={styles.list}>
+            {benefitsData.map((el, index) => {
+                return (
+                    <li key={index} className={styles.item}>
+                        <span className={styles.iconBox}>
+                            <svg className={styles.icon}>
+                                <use href={el.img} />
+                            </svg>
+                        </span>
+
+                        <p className={styles.title}>{el.title}</p>
+                    </li>
+                );
+            })}
+        </ul>
+    );
 };
 
 export default CooperationBenefits;
---
+++ src/data/cooperationBenefits.js
@@ -0,0 +1,18 @@
+export const benefitsData = [
+    {
+        img: '/sprite.svg#icon-cookie',
+        title: "Натуральні інгредієнти найвищої якості",
+    },
+    {
+        img: '/sprite.svg#icon-cookie',
+        title: "Гнучкі та взаємовигідні умови співпраці",
+    },
+    {
+        img: '/sprite.svg#icon-cookie',
+        title: "Сертифікована продукція",
+    },
+    {
+        img: '/sprite.svg#icon-cookie',
+        title: "Швидка доставка та зручний сервіс",
+    },
+]
