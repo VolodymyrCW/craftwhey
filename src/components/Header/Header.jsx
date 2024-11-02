df179850b05f85b04541ca754354c11a56fe9f0d
IvetaDuda <92565279+IvetaDuda@users.noreply.github.com>
2024-11-02 17:56:16 +0200

    header-styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,7 @@
 import BurgerBtn from '../buttons/BurgerBtn/BurgerBtn';
-import HeaderNavlinks from '../HeaderNavlinks/HeaderNavlinks';
-
-import styles from './Header.module.scss';
 import Logo from './Logo/Logo';
 import MobileNavigation from './mobileNavigation/MobileNavigation';
+import styles from './Header.module.scss';
 
 const Header = () => {
   return (
@@ -12,7 +10,17 @@ const Header = () => {
         <BurgerBtn />
         <Logo />
         <MobileNavigation />
-        <HeaderNavlinks className={styles.navLinks} />
+        <div className={styles.iconContainer}>
+          <svg className={styles.svg}>
+            <use href="sprite.svg/#icon-search_24" />
+          </svg>
+          <svg className={styles.svg}>
+            <use href="sprite.svg/#icon-cart_24" />
+          </svg>
+          <p className={styles.lang}>
+            <span>Укр</span> <span>Eng</span>
+          </p>
+        </div>
       </div>
     </header>
   );
--- src/components/HeaderNavlinks/HeaderNavlinks.jsx
+++ src/components/HeaderNavlinks/HeaderNavlinks.jsx
@@ -12,14 +12,22 @@ const HeaderNavlinks = ({ className }) => {
     <nav className={`${className} ${styles.navLinks}`}>
       <div className={styles.navList}>
         {navLinks.slice(0, 3).map((el) => (
-          <Link
-            key={el.title}
-            href={el.href ? el.href : ''}
-            onClick={el.title === 'Продукція' && handleReset}
-            className={styles.navLink}
-          >
-            {el.title}
-          </Link>
+          <div className={styles.linkContainer} key={el.title}>
+            <Link
+              href={el.href ? el.href : ''}
+              onClick={el.title === 'Продукція' && handleReset}
+              className={styles.navLink}
+            >
+              {el.title}
+            </Link>
+            {el.title !== 'Головна' && (
+              <button className={styles.arrowBtn}>
+                <svg>
+                  <use href="sprite.svg#icon-schevron_bottom" />
+                </svg>
+              </button>
+            )}
+          </div>
         ))}
       </div>
       {navLinks.map(
@@ -34,17 +42,6 @@ const HeaderNavlinks = ({ className }) => {
             </Link>
           )
       )}
-      <div className={styles.iconContainer}>
-        <svg className={styles.svg}>
-          <use href="sprite.svg/#icon-search_24" />
-        </svg>
-        <svg className={styles.svg}>
-          <use href="sprite.svg/#icon-cart_24" />
-        </svg>
-        <p className={styles.lang}>
-          <span>УКР</span> <span>РУС</span>
-        </p>
-      </div>
     </nav>
   );
 };
