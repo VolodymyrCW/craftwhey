9482ca07f4cc0b78a9865b54b40571a30c140337
PolztiukVolodymyr <92870556+PolztiukVolodymyr@users.noreply.github.com>
2024-12-11 16:47:37 +0200

    add empty basket

--- src/components/Basket/Basket.jsx
+++ src/components/Basket/Basket.jsx
@@ -2,13 +2,13 @@
 
 import { useContext, useState } from "react";
 import { SiteContext } from "@/context/siteContext";
+import EmptyBasket from "./EmptyBasket";
 
 import styles from "./Basket.module.scss";
 
 const Basket = () => {
     const { openBasket, setOpenBasket } = useContext(SiteContext);
     const [totalSum, setTotalSum] = useState(0);
-    // console.log("openBasket: ", openBasket);
 
     function handleBacketClose() {
         setOpenBasket(false);
@@ -22,27 +22,35 @@ const Basket = () => {
                     : `${styles.menu}`
             }
         >
-            {openBasket && (
-                <button onClick={handleBacketClose} className={styles.btnBack}>
-                    <svg className={styles.icon}>
-                        <use href='sprite.svg/#icon-schevron_right' />
-                    </svg>
-                </button>
+            {totalSum ? (
+                <div>
+                    {openBasket && (
+                        <button
+                            onClick={handleBacketClose}
+                            className={styles.btnBack}
+                        >
+                            <svg className={styles.icon}>
+                                <use href='sprite.svg/#icon-schevron_right' />
+                            </svg>
+                        </button>
+                    )}
+                    <div className={styles.cardHeader}>
+                        <svg className={styles.svg}>
+                            <use href='sprite.svg/#black_cart_32' />
+                        </svg>
+                        <h3 className={styles.cardTitle}>Ваше замовлення</h3>
+                    </div>
+                    <div className={styles.cardFooter}>
+                        <p className={styles.totalSum}>{totalSum}</p>
+
+                        <button className={styles.orderBtn}>
+                            Оформити замовлення
+                        </button>
+                    </div>
+                </div>
+            ) : (
+                <EmptyBasket />
             )}
-            <div className={styles.cardHeader}>
-                <svg className={styles.svg}>
-                    <use href='sprite.svg/#black_cart_32' />
-                </svg>
-                <h3 className={styles.cardTitle}>Ваше замовлення</h3>
-            </div>
-            <div className={styles.cardFooter}>
-                {totalSum ? (
-                    <p className={styles.totalSum}>{totalSum}</p>
-                ) : (
-                    <p className={styles.totalSum}>Ваш кошик пустий</p>
-                )}
-                <button className={styles.orderBtn}>Оформити замовлення</button>
-            </div>
         </div>
     );
 };
---
+++ src/components/Basket/EmptyBasket.jsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useContext } from "react";
+import { SiteContext } from "@/context/siteContext";
+
+import styles from "./Basket.module.scss";
+
+const EmptyBasket = () => {
+    const { openBasket, setOpenBasket } = useContext(SiteContext);
+
+    function handleBacketClose() {
+        setOpenBasket(false);
+    }
+    return (
+        <div className={styles.emptyBox}>
+            {openBasket && (
+                <button onClick={handleBacketClose} className={styles.btnBack}>
+                    <svg className={styles.icon}>
+                        <use href='sprite.svg/#icon-schevron_right' />
+                    </svg>
+                </button>
+            )}
+            <svg className={`${styles.svg} ${styles.svgEmptyBasket}`}>
+                <use href='sprite.svg/#black_cart_32' />
+            </svg>
+            <p className={styles.totalSum}>Ваш кошик пустий</p>
+        </div>
+    );
+};
+
+export default EmptyBasket;
